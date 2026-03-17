e7774069bb5c008ada242025ccf6560d39a3e864
Martin John M <johnmmartin74@gmail.com>
2026-03-17 11:21:32 +0100

    Created test for the multi level partial full fill sell order

--- apps/engine/src/trade/engine.rs
+++ apps/engine/src/trade/engine.rs
@@ -351,4 +351,26 @@ mod tests {
         let bids_at_98 = engine.orderbook.get_level_mut(dec!(98), Side::Bid).unwrap();
         assert_eq!(bids_at_98[0].quantity, dec!(2));
     }
+
+    #[test]
+    fn test_multi_level_full_fill_low_price_sell_order() {
+        let mut engine = MatchingEngine::new();
+
+        // Add bids 
+        engine.orderbook.add_order(Order { id: 1, quantity: dec!(1), price: dec!(101), side: Side::Ask });
+        engine.orderbook.add_order(Order { id: 2, quantity: dec!(1), price: dec!(102), side: Side::Ask });
+        engine.orderbook.add_order(Order { id: 3, quantity: dec!(1), price: dec!(103), side: Side::Ask });
+
+        // Add bids
+        engine.orderbook.add_order(Order { id: 4, quantity: dec!(4), price: dec!(99), side: Side::Bid });
+        engine.orderbook.add_order(Order { id: 5, quantity: dec!(5), price: dec!(98), side: Side::Bid });
+        engine.orderbook.add_order(Order { id: 6, quantity: dec!(1), price: dec!(97), side: Side::Bid });
+
+        // sent an sell order with quantity 9 and price : 96 (partial match for the best bid)
+        engine.process_order(Order { id:7, quantity: dec!(9), price: dec!(96), side: Side::Ask });
+
+        assert_eq!(engine.orderbook.best_bid(), Some(dec!(97)));
+        let bids_at_97 = engine.orderbook.get_level_mut(dec!(97), Side::Bid).unwrap();
+        assert_eq!(bids_at_97[0].quantity, dec!(1));
+    }
 }
