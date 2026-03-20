4ce485c87b35abae5f96f85d3e69beb805fa082f
Martin John M <johnmmartin74@gmail.com>
2026-03-20 10:00:22 +0100

    Created the cancel order logic

--- apps/engine/src/trade/orderbook.rs
+++ apps/engine/src/trade/orderbook.rs
@@ -2,7 +2,7 @@ use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
 use std::collections::{BTreeMap, HashMap, VecDeque};
 
-use crate::trade::model::{Order, Side};
+use crate::trade::model::{Order, OrderMetadata, Side};
 
 pub struct Orderbook {
     // Bids : Sorted descending (Highest price first)
@@ -44,6 +44,38 @@ impl Orderbook {
             .entry(order.price)
             .or_insert_with(|| VecDeque::new())
             .push_back(order);
+    }
+
+    pub fn cancel_order(&mut self, order_id : u64) -> bool {
+        // 1. find where the order is (0(1) Lookup in a hashmap)
+        // If the ID isnt in the lookup, its already filled or never existed
+
+        let metadata = match self.orders_lookup.remove(&order_id){
+            Some(m) => m,
+            None => return false
+        };
+
+        // 2. Go to the specific Price level in the correct Btreemap
+        let side_map = match metadata.side {
+            Side::Ask => &mut self.asks,
+            Side::Bid => &mut self.bids
+        };
+
+        if let(Some(queue)) = side_map.get_mut(&metadata.price) {
+            // 3. Remove the order from the VecDequeu
+            // retain keeps all elements EXCEPT the one with ID
+            queue.retain(|o| o.id != order_id);
+
+
+            // 4. CLEANUP: If this was the last order at this price,
+            // Remove the price key to save RAM 
+            if queue.is_empty() {
+                side_map.remove(&metadata.price);
+            }
+            return true;
+        } 
+
+        return true;
     }
 
     // Fn to get best bid (Highest)
