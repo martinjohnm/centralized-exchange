30c2d2fd82910f0f4ac5b7e8f204cbf117a58629
Martin John M <johnmmartin74@gmail.com>
2026-03-26 10:24:19 +0100

    Created the AssetCOnfig struct from the json file

--- apps/engine/src/utils.rs
+++ apps/engine/src/utils.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::format, u32};
+use std::{collections::HashMap, fmt::format, str, u32};
 
 use serde::Deserialize;
 
@@ -9,8 +9,24 @@ pub struct MarketConfig {
     pub quote: String,
     pub queue_prefix : String
 }
+#[derive(Deserialize, Debug, Clone)]
+pub struct AssetRegistry {
+    markets : HashMap<MarketId, InternalMarketConfig>,
+    asset_names: HashMap<AssetId, String>,
+    symbol_to_id : HashMap<String, AssetId>
+}
+#[derive(Deserialize, Debug, Clone)]
+pub struct InternalMarketConfig {
+    market_id: u32,
+    base_id : u32,
+    quote_id : u32,
+}
 
-pub fn load_markets() -> HashMap<String, MarketConfig> {
+type AssetPairName = String;
+type AssetId = u32;
+type MarketId = u32;
+
+pub fn load_markets() -> HashMap<AssetPairName, MarketConfig> {
     // This macro looks relative to the FILE it is written in.
     // From src/trade/model.rs, we need to go up 4 times to hit root.
     let data = include_str!("../../../markets.json"); 
@@ -33,6 +49,69 @@ impl MarketConfig {
         format!("{}", self.quote)
     }
 }
+
+
+pub fn initialize_registry() -> AssetRegistry {
+    
+    // 1. Load the raw JSON
+    let raw_data: HashMap<AssetPairName, MarketConfig> = load_markets();
+
+    // 2. Prepare the empty Registry
+    let mut registry = AssetRegistry {
+        markets : HashMap::new(),
+        asset_names: HashMap::new(),
+        symbol_to_id : HashMap::new()
+    };
+
+    // 3. Temporary state 
+    let mut asset_counter :  AssetId  = 1;
+    let mut market_counter:  MarketId = 1;
+
+    // this map ensures "USDT" gets the same ID regardless of market
+    let mut name_to_asset_id: HashMap<String, AssetId> = HashMap::new();
+
+    for (symbol, config) in raw_data {
+        // ---- CLosure: The ID generator--------------------
+        // This logic checks if an asset (like "BTC") already has a number.
+        // If not, it assigns the next available integer.
+
+        let mut get_asset = |name: &String| -> AssetId {
+            if let Some(&id) = name_to_asset_id.get(name) {
+                id
+            } else {
+                let id = asset_counter;
+                name_to_asset_id.insert(name.clone(), id);
+                registry.asset_names.insert(id, name.clone());
+                asset_counter += 1;
+                id
+            }
+        };
+
+        // --------- MAPPING --------------
+        let base_id = get_asset(&config.base);
+        let quote_id = get_asset(&config.quote);
+
+        let m_id = market_counter;
+
+        // Create the internal config
+        registry.markets.insert(m_id, InternalMarketConfig {
+            market_id : m_id,
+            base_id,
+            quote_id
+        });
+
+        // Map the human string "BTC_USDT" to the internal Id 1;
+        registry.symbol_to_id.insert(symbol, m_id);
+
+        market_counter += 1;
+
+    }
+
+    println!("✅ Registry Initialized: {} Markets, {} Assets", market_counter - 1, asset_counter - 1);
+    registry
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -50,47 +129,3 @@ mod tests {
         // assert!(markets.contains_key("BTC-USD"));
     }
 }
-
-pub fn initialize_registry()  {
-    let raw_data: HashMap<String, MarketConfig> = load_markets();
-
-    for r in raw_data {
-        println!("{:?}", r);
-    }
-    // let mut registry = AssetRegistry {
-    //     markets: HashMap::new(),
-    //     asset_names: HashMap::new(),
-    //     symbol_to_id: HashMap::new(),
-    // };
-
-    // let mut asset_counter: AssetId = 1 as u32;
-    // let mut market_counter: MarketId = 1 as u32;
-    // let mut name_to_asset_id: HashMap<String, AssetId> = HashMap::new();
-
-    // for (symbol, config) in raw_data {
-    //     // Helper to get or create AssetId for "BTC", "USDT", etc.
-    //     let get_asset = |name: &String, counter: &mut AssetId, map: &mut HashMap<String, AssetId>, names: &mut HashMap<AssetId, String>| {
-    //         *map.entry(name.clone()).or_insert_with(|| {
-    //             let id = *counter;
-    //             names.insert(id, name.clone());
-    //             *counter += 1;
-    //             id
-    //         })
-    //     };
-
-    //     let base_id = get_asset(&config.base, &mut asset_counter, &mut name_to_asset_id, &mut registry.asset_names);
-    //     let quote_id = get_asset(&config.quote, &mut asset_counter, &mut name_to_asset_id, &mut registry.asset_names);
-
-    //     let m_id = market_counter;
-    //     registry.markets.insert(m_id, InternalMarketConfig {
-    //         market_id: m_id,
-    //         base_id,
-    //         quote_id,
-    //     });
-    //     registry.symbol_to_id.insert(symbol, m_id);
-    //     market_counter += 1;
-    // }
-
-    
-}
-
