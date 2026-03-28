bbd41d2f26237768080985be7161b4102dbdfcd1
Martin John M <johnmmartin74@gmail.com>
2026-03-28 10:46:10 +0100

    Skeleton for the publisher

--- apps/engine/src/publisher.rs
+++ apps/engine/src/publisher.rs
@@ -1,5 +1,14 @@
+use tokio::sync::mpsc;
+
+use crate::model::Trade;
+
 
 
 pub struct RedisPublisher {
+    pub receiver : mpsc::Receiver<Trade>,
+    pub redis_url : String
+}
+
+impl RedisPublisher {
     
 }
