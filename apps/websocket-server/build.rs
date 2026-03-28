80b4bce7d06679bb8a99acaccbbd19a4628772a8
Martin John M <johnmmartin74@gmail.com>
2026-03-28 09:28:57 +0100

    added dependencies for the websocket server

---
+++ apps/websocket-server/build.rs
@@ -0,0 +1,15 @@
+
+
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 1. Tell Cargo to re-run if the proto file changes
+    println!("cargo:rerun-if-changed=../../proto/exchange.proto");
+
+    // 2. Compile the proto file
+    // First arg: path to the .proto file
+    // Second arg: the directory to search for imports
+    prost_build::compile_protos(&["../../proto/exchange.proto"], &["../../proto/"])?;
+    
+    Ok(())
+}
