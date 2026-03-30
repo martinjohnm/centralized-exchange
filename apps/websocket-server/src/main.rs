92817a0cc874602b155b26ef7caaf88d82c99f28
Martin John M <johnmmartin74@gmail.com>
2026-03-30 16:03:47 +0200

    Added the pubsub and btcusdt

--- apps/websocket-server/src/main.rs
+++ apps/websocket-server/src/main.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
-use axum::{Router, extract::{WebSocketUpgrade, ws::WebSocket}, response::Response, routing::get};
+use axum::{Router, extract::{State, WebSocketUpgrade, ws::{Message, WebSocket}}, response::Response, routing::get};
+use futures_util::StreamExt;
 use tokio::sync::broadcast;
 
 
@@ -23,21 +24,23 @@ async fn main() {
     let redis_tx = state.tx.clone();
 
     tokio::spawn(async move {
-        let client = redis::Client::open("redis:://127.0.0.1/").expect("Redis connection failed");
+        let client = redis::Client::open("redis://127.0.0.1:6379/").expect("Redis connection failed");
         let mut conn = client.get_multiplexed_async_connection().await.expect("Redis connetcon failde");
         let mut pubsub = client
             .get_async_pubsub()
             .await
             .expect("Pubsub connection failed");
 
-        pubsub.subscribe("trades:btcusdt").await.unwrap();
+        pubsub.subscribe("trades:btcusdt").await.expect("trades:btcusdt connection failed");
 
         
+
     });
 
     // 1. simple Axum router
     let app = Router::new()
-        .route("/ws", get(handler));
+        .route("/ws", get(handler))
+        .with_state(state);
 
     // 2. create a tcp listener
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
@@ -49,22 +52,40 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler(ws : WebSocketUpgrade) -> Response {
-    ws.on_upgrade(handle_socket)
+async fn handler(
+    ws : WebSocketUpgrade, 
+    State(state): State<Arc<AppState>>
+) -> Response {
+    ws.on_upgrade(move |socket| handle_socket(socket, state))
 }
 
-async fn handle_socket(mut socket: WebSocket) {
+async fn handle_socket(mut socket: WebSocket, state: Arc<AppState>) {
     println!("Connection establishes");
+    let mut rx = state.tx.subscribe();
 
-
+    // 5. THE REAL-TIME LOOP
+    // We wait for either a trade from Redis OR a 10s Heartbeat
+    let mut heartbeat_interval = tokio::time::interval(tokio::time::Duration::from_secs(10));
     // Test loop
+    
+    println!("{:?}", 34);
     loop {
-        let heartbeat = vec![1,2,3,4,5];
+        tokio::select! {
+            // Receive trade from the broadcast channel
+            Ok(bytes) = rx.recv() => {
 
-        if socket.send(axum::extract::ws::Message::Binary(heartbeat.into())).await.is_err() {
-            break;
+
+                if socket.send(Message::Binary(bytes.into())).await.is_err() {
+                    break; 
+                }
+
+            }
+            // Send Heartbeat Ping to keep connection alive
+            _ = heartbeat_interval.tick() => {
+                if socket.send(Message::Ping(vec![].into())).await.is_err() {
+                    break;
+                }
+            }
         }
-
-        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
     }
 }
