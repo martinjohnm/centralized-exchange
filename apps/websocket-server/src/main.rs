fdda534f77b8ac347eb3d01c83bd6429225948f1
Martin John M <johnmmartin74@gmail.com>
2026-03-30 15:13:54 +0200

    adeed the pubsub layer

--- apps/websocket-server/src/main.rs
+++ apps/websocket-server/src/main.rs
@@ -1,4 +1,7 @@
+use std::sync::Arc;
+
 use axum::{Router, extract::{WebSocketUpgrade, ws::WebSocket}, response::Response, routing::get};
+use tokio::sync::broadcast;
 
 
 
@@ -6,10 +9,32 @@ mod model;
 mod state;
 mod handler;
 
-
+struct AppState {
+    tx : broadcast::Sender<Vec<u8>>
+}
 
 #[tokio::main]
 async fn main() {
+
+    // 2. create a broadcast channel 
+    let (tx, _rx) = broadcast::channel::<Vec<u8>>(1024);
+    let state = Arc::new(AppState { tx });
+    // 3. spawn a background green thread for redis litening incoming messages
+    let redis_tx = state.tx.clone();
+
+    tokio::spawn(async move {
+        let client = redis::Client::open("redis:://127.0.0.1/").expect("Redis connection failed");
+        let mut conn = client.get_multiplexed_async_connection().await.expect("Redis connetcon failde");
+        let mut pubsub = client
+            .get_async_pubsub()
+            .await
+            .expect("Pubsub connection failed");
+
+        pubsub.subscribe("trades:btcusdt").await.unwrap();
+
+        
+    });
+
     // 1. simple Axum router
     let app = Router::new()
         .route("/ws", get(handler));
