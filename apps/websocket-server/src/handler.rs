9f7954b9c49ac2a819667ecea5d8ede8efe94adf
Martin John M <johnmmartin74@gmail.com>
2026-04-01 19:26:48 +0200

    Added legs skeleton for the socket handler

--- apps/websocket-server/src/handler.rs
+++ apps/websocket-server/src/handler.rs
@@ -8,8 +8,14 @@ use axum::{extract::{ws::{Message as WsMessage, WebSocket}}};
 pub async fn handle_socket(mut socket: WebSocket, state: Arc<AppState>) {
 
     let mut subscriptions = StreamMap::new();
-    
+    let mut heartbeat = tokio::time::interval(tokio::time::Duration::from_secs(10));
+
     loop {
+        tokio::select! {
+            // Leg 1. Receive json from frontend
+            // Leg 2. Receive Data from the subscribed market
+            // Leg 3. Heartbeat
+        }
         
     }
 }
