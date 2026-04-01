1fce2acb5dfb44a09fe7e3e0c35c98b9c41e7c37
Martin John M <johnmmartin74@gmail.com>
2026-04-01 19:22:39 +0200

    Added tokio-stream

--- apps/websocket-server/src/handler.rs
+++ apps/websocket-server/src/handler.rs
@@ -1,46 +1,15 @@
 use std::sync::Arc;
 
 use tokio::sync::broadcast;
+use tokio_stream::StreamMap;
 use crate::AppState;
 use axum::{extract::{ws::{Message as WsMessage, WebSocket}}};
 
 pub async fn handle_socket(mut socket: WebSocket, state: Arc<AppState>) {
-    println!("Connection establishes");
-    let mut rx = state.tx.subscribe();
 
-    // 5. THE REAL-TIME LOOP
-    // We wait for either a trade from Redis OR a 10s Heartbeat
-    let mut heartbeat_interval = tokio::time::interval(tokio::time::Duration::from_secs(10));
-    // Test loop
-    
-    let mut ticker = tokio::time::interval(tokio::time::Duration::from_secs(1));
-    
+    let mut subscriptions = StreamMap::new();
     
     loop {
-        tokio::select! {
-            // 🚀 Capture the result of the receive
-            res = rx.recv() => {
-                match res {
-                    Ok(bytes) => {
-                        if socket.send(WsMessage::Binary(bytes.into())).await.is_err() {
-                            println!("🔌 Client disconnected");
-                            break; 
-                        }
-                    }
-                    // ⚠️ This happens if the engine is too fast for the websocket
-                    Err(tokio::sync::broadcast::error::RecvError::Lagged(n)) => {
-                        eprintln!("⚠️ Client lagged by {} messages", n);
-                        // We don't break, but we acknowledge the lag
-                    }
-                    Err(_) => break, // Channel closed
-                }
-            }
-            
-            _ = heartbeat_interval.tick() => {
-                if socket.send(WsMessage::Ping(vec![].into())).await.is_err() {
-                    break;
-                }
-            }
-        }
+        
     }
 }
