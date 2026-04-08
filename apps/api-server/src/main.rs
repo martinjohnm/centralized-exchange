5ed3c6cdbf73bdf9da33f7fca954de317ee08a6b
Martin John M <johnmmartin74@gmail.com>
2026-04-08 10:20:24 +0200

    Added the axum server handler to the api-server

--- apps/api-server/src/main.rs
+++ apps/api-server/src/main.rs
@@ -1,10 +1,26 @@
-use std::error::Error;
+use std::{error::Error, net::SocketAddr};
+
+use axum::{Router, routing::get};
 
 
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
+    
+    let app = Router::new()
+        .route("/", get(handler));
+
+    let addr = SocketAddr::from(([0,0,0,0], 3000));
+    println!("listening on {:?}", addr);
+
+    // run the server 
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    
     Ok(())
+}
+
+async fn handler() -> &'static str {
+    "Hello john"
 }
