8525bb5a5f7e343f011217b9165d837bc5b46a34
Martin John M <johnmmartin74@gmail.com>
2026-04-11 12:43:17 +0200

    feat(frontend) - Resolved the httpclient to link fetch klines

--- apps/frontend/src/utils/httpClient.ts
+++ apps/frontend/src/utils/httpClient.ts
@@ -4,7 +4,7 @@ const BE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
 
 
 export async function getKlines(symbol : string, interval: string, limit: number) {
-    const response = await axios.get(`${BE_URL}/get-klines?symbol=${symbol}_3&interval=${interval}&limit=${limit}`)
+    const response = await axios.get(`${BE_URL}/get-klines?symbol=${symbol}&interval=${interval}&limit=${limit}`)
     const data = response.data;
     console.log(data);
     
